1e2d550a5fb825fec58a9aab03cff0fb9f69e100
Bram Gerritsen <bgerritsen@hotmail.com>
2013-12-02 09:46:11 +0100

    Initial commit

---
+++ Module.php
@@ -0,0 +1,60 @@
+<?php
+/**
+ *
+ */
+namespace StrokerTemplateInspector;
+
+use StrokerTemplateInspector\View\Renderer\PhpRenderer;
+use Zend\ModuleManager\Feature\AutoloaderProviderInterface;
+use Zend\ModuleManager\Feature\ConfigProviderInterface;
+use Zend\ModuleManager\Feature\ServiceProviderInterface;
+use Zend\View\Strategy\PhpRendererStrategy;
+
+class Module implements ConfigProviderInterface, ServiceProviderInterface, AutoloaderProviderInterface
+{
+    /**
+     * Returns configuration to merge with application configuration
+     *
+     * @return array|\Traversable
+     */
+    public function getConfig()
+    {
+        return include __DIR__ . '/config/module.config.php';
+    }
+
+    /**
+     * Expected to return \Zend\ServiceManager\Config object or array to
+     * seed such an object.
+     *
+     * @return array|\Zend\ServiceManager\Config
+     */
+    public function getServiceConfig()
+    {
+        return array(
+            'factories' => array(
+                'StrokerTemplateStrategy' => function($sm) {
+                    $renderer = new PhpRenderer;
+                    $renderer->setHelperPluginManager($sm->get('ViewHelperManager'));
+                    $renderer->setResolver($sm->get('ViewResolver'));
+                    return new PhpRendererStrategy($renderer);
+                }
+            )
+        );
+    }
+
+    /**
+     * Return an array for passing to Zend\Loader\AutoloaderFactory.
+     *
+     * @return array
+     */
+    public function getAutoloaderConfig()
+    {
+        return array(
+            'Zend\Loader\StandardAutoloader' => array(
+                'namespaces' => array(
+                    __NAMESPACE__ => __DIR__ . '/src/' . __NAMESPACE__,
+                ),
+            ),
+        );
+    }
+}
---
+++ src/StrokerTemplateInspector/View/Renderer/PhpRenderer.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * PhpRenderer
+ *
+ * @category  StrokerTemplateInspector\View\Renderer
+ * @package   StrokerTemplateInspector\View\Renderer
+ * @copyright 2013 ACSI Holding bv (http://www.acsi.eu)
+ * @version   SVN: $Id$
+ */
+
+namespace StrokerTemplateInspector\View\Renderer;
+
+use \Zend\View\Renderer\PhpRenderer as BasePhpRenderer;
+
+class PhpRenderer extends BasePhpRenderer
+{
+    public function render($nameOrModel, $values = null)
+    {
+        $content = parent::render($nameOrModel, $values);
+        if (is_string($nameOrModel)) {
+            $content .= '<div style="z-index: 99999; border: 1px solid red; background: yellow; padding: 2px; position: absolute;">' . $nameOrModel . '</div>';
+        }
+        return $content;
+    }
+}
